a8c1d145f70bc39dea3f96044d952d9f270f7956
Anas bitar <a.bitar@saedubai.com>
2016-09-27 18:55:25 +0400

    timer optimization: Instead of Update used a courtoune

--- naptime/Assets/_Scripts/GameManager.cs
+++ naptime/Assets/_Scripts/GameManager.cs
@@ -29,31 +29,31 @@ public class GameManager : MonoBehaviour {
 		g_gameManager = this;
 
 		StartCoroutine (Fade (Color.black, Color.clear, 1));
-		//StartCoroutine (Timer());
+		StartCoroutine (Timer());
 	}
 
 	void Update()
 	{
-		// Timer in Minutes and seconds. 
-		startTime += Time.deltaTime;
-		// Check for 1 seconds and add a second. also check if the timer is being paused or not. because the timer will be paused by using a time powerup or at 2.5D Mode.
-		if (startTime >=1 && isTimerPaused == false )
-		{
-			seconds++;
-			//Reset the starttime to 0 so that we calculate a new second.
-			startTime = 0;
-			// when seconds get's to 60 means we got a full minitue
-			if (seconds >= 59)
-			{
-				// Add our minute 
-				minutes++;
-				// reset seconds so we calculate a new minute.
-				seconds = 0;
-
-			}
-			// Update our text.
-			timeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
-		}
+//		// Timer in Minutes and seconds. 
+//		startTime += Time.deltaTime;
+//		// Check for 1 seconds and add a second. also check if the timer is being paused or not. because the timer will be paused by using a time powerup or at 2.5D Mode.
+//		if (startTime >=1 && isTimerPaused == false )
+//		{
+//			seconds++;
+//			//Reset the starttime to 0 so that we calculate a new second.
+//			startTime = 0;
+//			// when seconds get's to 60 means we got a full minitue
+//			if (seconds >= 59)
+//			{
+//				// Add our minute 
+//				minutes++;
+//				// reset seconds so we calculate a new minute.
+//				seconds = 0;
+//
+//			}
+//			// Update our text.
+//			timeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+		//}
 	}
 
 	void LevelEnded()
@@ -67,29 +67,33 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-//	IEnumerator Timer()
-//	{
-//		while (true && isTimerPaused == false)
-//		{
-//				
-//
-//			yield return new WaitForSeconds(1);
-//			seconds++;
-//
-//			if (seconds >= 59)
-//				{
-//					minutes++;
-//					seconds = 0;
-//
-//				}
-//
-//				//minutes = (int)(Time.deltaTime / 60);
-//				//seconds = (int)(Time.deltaTime % 60);
-//				timeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
-//
-//
-//		}
-//	}
+	IEnumerator Timer()
+	{
+		while (true )
+		{
+				
+
+			yield return new WaitForSeconds(1);
+
+			if (isTimerPaused == false)
+			{
+				seconds++;
+
+				if (seconds >= 59)
+				{
+					minutes++;
+					seconds = 0;
+
+				}
+
+				//minutes = (int)(Time.deltaTime / 60);
+				//seconds = (int)(Time.deltaTime % 60);
+				timeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+			}
+
+		
+		}
+	}
 
 
 	IEnumerator Fade(Color a_from, Color a_to, float a_time)
