37dc56ffd2d028c5d81066f4a16d54c8a14b2ac3
Anas bitar <a.bitar@saedubai.com>
2016-11-01 13:28:47 +0400

    Summary: A whole new car behavior added to map1! the car will even go faster if u press "spacebar" and it will respawn if u collide! Added: -Car behavior (Now the player can ride a car in this long path in map 1) -Exit the car in the end
    Updates: Will add new obstacles that works perfectly with the car behavior

---
+++ naptime/Assets/_Scripts/Car/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+	public float speed = 40;
+
+	GameObject player = null;
+	bool isRiding = false;
+
+	public GameObject secondCam;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (player && player.activeInHierarchy && Input.GetKeyDown(KeyCode.E))
+		{
+			isRiding = true;
+			player.SetActive(false);
+			FindObjectOfType<TrackObject>().target = transform;
+		}
+
+		if (isRiding)
+		{
+			Vector3 pos = transform.position;
+			FindObjectOfType<TrackObject>().enabled = false;
+			//FindObjectOfType<Camera>().transform.parent = this.transform;
+			secondCam.SetActive (true);
+			// = this.transform;
+
+
+			pos.z += Mathf.Lerp(speed, speed * 2, Input.GetAxis("Jump")) * Time.deltaTime;
+			pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+			transform.position = pos;
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.GetComponent<Player>())
+		{
+			player = collider.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.GetComponent<Player>())
+		{
+			player = null;
+		}
+	}
+
+	public void GetOutOfTheCar()
+	{
+		secondCam.SetActive (false);
+
+		player.SetActive(true);
+		FindObjectOfType<TrackObject>().enabled = true;
+
+		player.transform.position = transform.position + Vector3.forward * 5;
+		isRiding = false;
+
+		FindObjectOfType<TrackObject>().target = player.transform;
+	}
+
+	public void Teleport(Transform point)
+	{
+		transform.position = point.position;
+	}
+}
---
+++ naptime/Assets/_Scripts/Car/ColliderTeleport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTeleport : MonoBehaviour
+{
+	public Transform respawnPoint;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		collider.SendMessage("Teleport", respawnPoint, SendMessageOptions.DontRequireReceiver);
+	}
+}
